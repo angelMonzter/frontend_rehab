d75c8185d160a6ab0b897b12416828cec87135a5
angelMonzter <angelmonztr2000@hotmail.com>
2025-03-11 15:03:43 -0600

    desplegar frontend build

--- src/context/PacienteProvider.jsx
+++ src/context/PacienteProvider.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect, createContext } from "react";
 import axiosInstance from '../config/axios';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:4000'); // Asegúrate de que la URL coincide con tu backend
+const socket = io(process.env.REACT_APP_API_URL); // Asegúrate de que la URL coincide con tu backend
 
 const PacienteContext = createContext();
 
